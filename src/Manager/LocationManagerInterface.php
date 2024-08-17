01847b0d5cfc419e18b44dc0069b2cda9bfcc512
Khalimov Renato <bredevil@Khalimovs-MBP.fritz.box>
2024-08-17 23:06:21 +0200

    implemented processing of current user location

---
+++ src/Manager/LocationManagerInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Manager;
+
+use App\Entity\Location as LocationEntity;
+use Luzrain\TelegramBotApi\Type\Message;
+
+interface LocationManagerInterface
+{
+    public function process(Message $message): LocationEntity;
+}
